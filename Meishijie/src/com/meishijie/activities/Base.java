da054893dfe96f77d4a6f4c6856231cb28a5051a
mydha <dha.2007102225@gmail.com>
2014-02-27 12:15:38 +0800

    add data

--- Meishijie/src/com/meishijie/activities/Base.java
+++
@@ -1,7 +0,0 @@
-package com.meishijie.activities;
-
-import android.app.Activity;
-
-public class Base extends Activity{
-
-}
---
+++ Meishijie/src/com/meishijie/activities/Splash.java
@@ -0,0 +1,19 @@
+package com.meishijie.activities;
+
+import java.io.FileInputStream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	public void copyDatabase(){
+		FileInputStream fis = null;
+	}
+	
+}
