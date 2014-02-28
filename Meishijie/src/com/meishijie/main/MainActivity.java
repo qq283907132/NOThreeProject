ed95bc474a4ad2a8bf99478f58d7a35a1e06ba52
wuxin2014 <zmyan0205@163.com>
2014-02-28 16:21:04 +0800

    change

--- Meishijie/src/com/meishijie/main/MainActivity.java
+++ Meishijie/src/com/meishijie/main/MainActivity.java
@@ -1,24 +1,16 @@
 package com.meishijie.main;
 
-import com.meishijie.activities.SearchActivity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
 	}
 
-
-	public void jump(View btn){
-		Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-		startActivity(intent);
-	}
 
 }
