c1832c4dc9e09eac065197f2d5cb8f3a95266a9a
wuxin2014 <zmyan0205@163.com>
2014-03-06 11:26:29 +0800

    update NewsContentActivity

--- SplashActivity/src/com/meishijie/activities/NewsContentActivity.java
+++ SplashActivity/src/com/meishijie/activities/NewsContentActivity.java
@@ -14,6 +14,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -32,16 +34,15 @@ public class NewsContentActivity extends Activity implements OnScrollListener {
 	// 放置记录的总条数
 	private List<NewsContent> total = null;
 	// 是否进行分页操作
-	private boolean is_divpage;
-	// 页码
-	private static int pageNo = 0;
-	// 每一页要显示的数据
-	private int number = 10;
-	private MyListTask task;
+	private int number = 10;//每次获取多少条数据
+	//是否加载完成
+    private boolean loadfinish = true;
+    
 	private View header;
 	private View footer;
 	private TextView title;
 	private TextView desc;
+	private List<NewsContent> contentList;
 	
 
 	@Override
@@ -49,14 +50,11 @@ public class NewsContentActivity extends Activity implements OnScrollListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fooder_menu);
 		
+		total = new ArrayList<NewsContent>();
+		
 		Intent intent = getIntent();
 		bclassname = intent.getStringExtra("bclassname");
 		classList = (List<NewsClass>) intent.getSerializableExtra("classList");
-		
-		
-		total = new ArrayList<NewsContent>();
-		pageNo = 1;
-		task = new MyListTask();
 		
 		title = (TextView) findViewById(R.id.title);
 		title.setText(bclassname);
@@ -69,6 +67,7 @@ public class NewsContentActivity extends Activity implements OnScrollListener {
 		footer = LayoutInflater.from(this).inflate(R.layout.list_more,
 				listView, false);
 		listView.addFooterView(footer);
+		
 		adapter = new FoodAdapter(this);
 		
 		contentDao = new NewsContentDaoImpl(this);
@@ -80,93 +79,87 @@ public class NewsContentActivity extends Activity implements OnScrollListener {
 
 	private void initView(String bclassname, List<NewsClass> classList) {
 		if ("中华菜系".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("外国菜谱".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("菜谱大全".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("营养健康".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("烘焙".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		}else if ("厨房百科".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("人群膳食".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		}else if ("功能性调理".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("疾病调理".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		} else if ("脏腑调理".equals(bclassname)) {
-			task.execute();
+			setListViewData(bclassname,classList);
 		}
 	}
-
-	public class MyListTask extends AsyncTask<Void, Void, List<NewsContent>> {
-
-		private int maxPage;
-		private List<NewsContent> contentList;
-
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-		}
-
-		@Override
-		protected List<NewsContent> doInBackground(Void... params) {
-			int count = contentDao.getSuperSum(bclassname);
-			Log.i("newscontent" , "count:"+count);
-			int maxPage = count%number == 0 ? count/number : count/number+1;
-			Log.i("newscontent" , "maxPage:"+maxPage);
-			if(count < number){
-				number = count;
-			}
-			if(pageNo<=maxPage){
-				if(pageNo==maxPage && count%number!=0){
-					number = count%number;
-				}
-				contentList = contentDao
-						.getPartNewsContentBySuperName(bclassname, (pageNo - 1)
-								* number + "," + number);
-			}
-			return contentList;
-		}
-
-		@Override
-		protected void onPostExecute(List<NewsContent> result) {
-			super.onPostExecute(result);
-			total.addAll(result);
-			adapter.setData(total);
-			if (pageNo == 1) {
-				listView.setAdapter(adapter);
-			}
-			adapter.notifyDataSetChanged();
-			pageNo++;
-		}
-
+	
+	public void setListViewData(String bclassname, List<NewsClass> classList){
+		contentList = contentDao
+				.getPartNewsContentBySuperName(bclassname,"0,10");
+		total.addAll(contentList);
+		adapter.setData(total);
+		listView.setAdapter(adapter);
 	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == event.KEYCODE_BACK) {
-			task.cancel(true);
-			finish();
-		}
-		return true;
-	}
+	
+	private Handler handler = new Handler(){
+		public void handleMessage(Message msg) {
+			total.addAll((List<NewsContent>)msg.obj);
+			adapter.notifyDataSetChanged();//告诉ListView数据已经发生改变，要求ListView更新界面显示
+			if(listView.getFooterViewsCount() > 0) listView.removeFooterView(footer);
+			loadfinish = true;
+		}    	
+    };
 
 	@Override
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
-		is_divpage = (firstVisibleItem + visibleItemCount == totalItemCount);
+		
+		int count = contentDao.getSuperSum(bclassname);
+		Log.i("newscontent" , "count:"+count);
+		int maxpage = count%number == 0 ? count/number : count/number+1;
+		Log.i("newscontent" , "maxpage:"+maxpage);
+		
+		final int loadtotal = totalItemCount;
+		int lastItemid = listView.getLastVisiblePosition();//获取当前屏幕最后Item的ID
+		if((lastItemid+1) == totalItemCount){//达到数据的最后一条记录
+			if(totalItemCount > 0){
+				//当前页
+				int currentpage = totalItemCount%number == 0 ? totalItemCount/number : totalItemCount/number+1;
+				int nextpage = currentpage + 1;//下一页
+				if(nextpage <= maxpage && loadfinish){
+					loadfinish = false;
+					listView.addFooterView(footer);
+					
+					new Thread(new Runnable() {						
+						public void run() {
+							try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							List<NewsContent> result = contentDao.getPartNewsContentBySuperName(bclassname,loadtotal+","+number);
+							handler.sendMessage(handler.obtainMessage(100, result));
+						}
+					}).start();
+				}		
+			}
+					
+		}
 	}
 
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		if (is_divpage && scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
-			listView.addFooterView(footer);
-			new MyListTask().execute();
-		}
+	
 	}
+	
+	
 
 }
